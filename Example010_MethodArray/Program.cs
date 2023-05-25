cf1628f796e30a07b695255e268149cd5b267303
Fedor Romanov <flauto86@gmail.com>
2023-05-25 15:01:11 +0300

    8-11

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 13, 42, 53, 4, 85, 4, 66, 47, 28 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
